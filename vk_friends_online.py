1783dd82630c80f787c4cdbb9a86167818e573be
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-02-02 16:32:55 +0300

    pep8

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -22,9 +22,9 @@ def get_online_friends(login, password):
             scope='friends'
         )
         api = vk.API(session)
-        online_id_list = api.friends.getOnline()
-        if online_id_list:
-            friends_online = api.users.get(user_ids=online_id_list)
+        friends_online_ids = api.friends.getOnline()
+        if friends_online_ids:
+            friends_online = api.users.get(user_ids=friends_online_ids)
         else:
             friends_online = []
         return friends_online
@@ -35,17 +35,16 @@ def output_friends_to_console(friends_online):
         print('Nobody is online at the moment')
     else:
         print('Friends online: ')
-        for friend_name in friends_online:
-            print('{} {}'.format(friend_name['first_name'],
-                                 friend_name['last_name']))
+        for friend in friends_online:
+            print('{} {}'.
+                  format(friend['first_name'], friend['last_name']))
 
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-
     try:
         friends_online = get_online_friends(login, password)
         output_friends_to_console(friends_online)
-    except vk.exceptions.VkAuthError as error:
-        print(error)
+    except vk.exceptions.VkAuthError:
+        pass
